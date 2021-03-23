ed5c56a31930c2806816da98074a728a7a475fdd
Jackson Rosser <jacksonrosser@Jacksons-MacBook-Pro.local>
2021-03-22 20:05:09 -0600

    URL

--- 02-InsepectorClouseau/javascript/main.js
+++ 02-InsepectorClouseau/javascript/main.js
@@ -5,10 +5,7 @@ function reDisplay() {
 let windowSizes = `The Window size is ${window.innerWidth}px wide by ${window.innerHeight}px tall.`
 
 let offset = `Window offset ${window.screenX}px from the left edge and ${window.screenY}px from the top of the display.`
-
-let myWindow = document.querySelector("#windowProp").innerText= windowSizes + '\n' + offset
+let urlInfo = `This page's URL is ${window.location}`
+let myWindow = document.querySelector("#windowProp").innerText= windowSizes + '\n' + offset + '\n' + urlInfo
 } 
-
-
-let urlInfo = document.querySelector("#documentProp").innerText= `The page URL is ${Location.href}`
 reDisplay()
